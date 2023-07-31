8486dc11489ae40c61694d56076957691ef621b1
dhruuuuv <75282257+dhruuuuv@users.noreply.github.com>
2023-07-31 23:34:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const InputBox=document.getElementById("input-box");
+const ListContainer=document.getElementById("list-container");
+
+function AddTask(){
+    if(InputBox.value === ''){
+        alert("You are writing something!");
+
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=InputBox.value;
+        ListContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    InputBox.value="";
+    saveData();
+}
+
+ListContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",ListContainer.innerHTML);
+}
+function showTask(){
+    ListContainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
+  
